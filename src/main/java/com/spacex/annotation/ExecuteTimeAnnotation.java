50124d8a4619a4765db4f9dbcfc49ef6a136c7eb
Lucas Ley <ayaaa168@163.com>
2018-07-06 10:15:30 +0800

    add execute time aspect with springboot aop

---
+++ src/main/java/com/spacex/annotation/ExecuteTimeAnnotation.java
@@ -0,0 +1,11 @@
+package com.spacex.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ExecuteTimeAnnotation {
+}
