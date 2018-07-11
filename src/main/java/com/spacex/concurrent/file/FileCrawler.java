df3a99115b7533b2e1da4443a163e971e0f8164e
Lucas Ley <ayaaa168@163.com>
2018-07-11 10:38:15 +0800

    add concurrent crawler

---
+++ src/main/java/com/spacex/concurrent/file/FileCrawler.java
@@ -0,0 +1,46 @@
+package com.spacex.concurrent.file;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.util.concurrent.BlockingQueue;
+
+public class FileCrawler implements Runnable {
+
+    private final BlockingQueue<File> fileQueue;
+    private final FileFilter fileFilter;
+    private final File root;
+
+
+    public FileCrawler(BlockingQueue<File> fileQueue, FileFilter fileFilter, File root) {
+        this.fileQueue = fileQueue;
+        this.fileFilter = fileFilter;
+        this.root = root;
+    }
+
+    @Override
+    public void run() {
+        try {
+            crawl(root);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private void crawl(File root) throws InterruptedException {
+        File[] entries = root.listFiles(fileFilter);
+        if (entries != null) {
+            for (File entry : entries) {
+                if (entry.isDirectory()) {
+                    crawl(entry);
+                } else if (!alreadyIndexed(entry)) {
+                    fileQueue.put(entry);
+                }
+            }
+        }
+    }
+
+    private boolean alreadyIndexed(File entry) {
+        return true;
+    }
+}
